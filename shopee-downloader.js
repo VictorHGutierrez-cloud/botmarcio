bbb73cda45387ecc46f27587d2ed66e68897adb7
VictorHGutierrez-cloud <victor.henrique.duarte@alumni.usp.br>
2025-11-28 06:25:32 -0300

    Fix: Adicionar import do child_process

--- shopee-downloader.js
+++ shopee-downloader.js
@@ -3,6 +3,7 @@ const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
+const { execSync } = require('child_process');
 
 class ShopeeDownloader {
   constructor() {
